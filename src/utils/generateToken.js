723ce03320260862605dd015fc8c0f2e77f5d6e1
ToxicJoEz <latenightgamer20166@gmail.com>
2025-07-21 08:53:48 +0300

    updated token time

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { userId: user._id, email: user.email },
     process.env.JWT_SECRET,
-    { expiresIn: "1h" }
+    { expiresIn: "1d" }
   );
 };
 
