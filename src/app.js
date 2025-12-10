19988da4773115bae97dd8ea55cbbe69f2947e41
ToxicJoEz <latenightgamer20166@gmail.com>
2025-12-10 11:02:31 +0200

    fixed logs3

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ const app = express();
 
 // Global middleware
 app.use(cors());
-app.use(express.json());
+// Increase payload size limit to handle large search logs
+// The default is '100kb', which is too small.
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Connect to MongoDB
 mongoose
